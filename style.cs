e4f6091b1471595ed1f64eaa49e27915e24b2740
rhae <Kiwi.Sudduth@gmail.com>
2024-09-16 17:00:56 -0700

    test commit
    I added new images and created a new tuxedo wiki page.

---
+++ style.cs
@@ -0,0 +1,20 @@
+h1{
+    color:rgb(240, 152, 125);
+    border: 2px solid;
+    border-radius:10px;
+    font-size:200px; 
+}
+
+p{
+    background-color:rgb(172, 209, 241);
+}
+
+img{
+    height:150px;
+    width:150px;
+}
+
+ol{
+    border:4px;
+    background-color:aquamarine;
+}
